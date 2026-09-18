558e1fa70b2cc292ad3b32affcda07720ca6d329
Seongsu Park <hotgogohotb@gmail.com>
2026-09-18 21:40:40 +0900

    Fix: Add postcss config for Tdfghailwind CSS

--- soccer-score-app/src/App.jsx
+++ soccer-score-app/src/App.jsx
@@ -1,5 +1,5 @@
 // 구글 앱스 스크립트 웹앱 URL
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwu39gCSo49ZFWyxgvNuoiw3Np7RvB2HbtfAmRX4uTJ0CF1fvM2BPMVl9pUFgQDG5et/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbyJAwZD_k69nTmsFftPldcVPWtXfyUqqJIV4PYYeAq6UoPdWaU9D4fz6Kvmb6qBBl0Z/exec";
 const GAS_URL2  = "https://script.google.com/macros/s/AKfycbzb-Tr6EnOa5FORkuiP6KrUif5emEzDS_S-XlQMfF_uIS9ZdXs_4XkJG28SXRp034Ed/exec";
 
 export default function QuickScoreTracker() {
